c0f480bf0a46a98e2715c02398dc08397c34e309
Siyaman <siyaman.sadagopan@bytedance.com>
2025-08-28 13:02:25 -0700

    Add second page
    Add React router and another page

---
+++ src/SecondPage.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function SecondPage() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Second Page</h1>
+      <div className="card">
+        <button onClick={() => setCount(count => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  );
+}
