680eb6f599328b23c6924a2ae46e623a2f7cc07d
Siyaman <siyaman.sadagopan@bytedance.com>
2025-08-29 15:43:13 -0700

    Add CoverFlow component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import FirstPage from "./FirstPage.tsx";
-import SecondPage from "./SecondPage.tsx";
+import CoverFlow from "./CoverFlow.tsx";
 
 export default function App() {
   return (
     <Router basename={__XR_ENV_BASE__}>
       <Routes>
-        <Route path="/second-page" element={<SecondPage />} />
-        <Route path="/" element={<FirstPage />} />
+        <Route path="/" element={<CoverFlow />} />
       </Routes>
     </Router>
   );
---
+++ src/CoverFlow.tsx
@@ -0,0 +1,36 @@
+import "./CoverFlow.css";
+
+const IMAGES = [
+  "/images/IMG_3394.JPG",
+  "/images/IMG_0412.jpg",
+  "/images/IMG_2981.HEIC",
+  "/images/IMG_3008.PNG",
+  "/images/IMG_3368.JPG",
+  "/images/IMG_3372.JPG",
+  "/images/IMG_3373.JPG",
+  "/images/IMG_3374.JPG",
+  "/images/IMG_3375.JPG",
+  "/images/IMG_3377.JPG",
+  "/images/IMG_3381.JPG",
+  "/images/IMG_3385.JPG",
+  "/images/IMG_3387.JPG",
+  "/images/IMG_3388.JPG",
+  "/images/IMG_3389.JPG",
+  "/images/IMG_3390.JPG",
+  "/images/IMG_3392.JPG",
+] as const;
+
+export default function CoverFlow() {
+  return (
+    <main className="coverFlow" enable-xr>
+      <h1>Hello Siyaman</h1>
+      <ul className="cards">
+        {IMAGES.map((image) => (
+          <li>
+            <img draggable="false" src={image} height="600" alt="Wedding" />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
