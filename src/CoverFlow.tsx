ffc9164f7815c81018565ae4db7d647918c3e839
Siyaman <siyaman.sadagopan@bytedance.com>
2025-09-02 00:29:55 -0700

    Move images to assets folder
    In AVP mode the base URL is different so loading from public folder is
    non trivial

--- src/CoverFlow.tsx
+++ src/CoverFlow.tsx
@@ -1,32 +1,39 @@
 import "./CoverFlow.css";
 
+import IMG_3394 from "./assets/IMG_3394.JPG";
+import IMG_0412 from "./assets/IMG_0412.jpg";
+import IMG_3008 from "./assets/IMG_3008.PNG";
+import IMG_3368 from "./assets/IMG_3368.JPG";
+import IMG_3372 from "./assets/IMG_3372.JPG";
+import IMG_3373 from "./assets/IMG_3373.JPG";
+import IMG_3374 from "./assets/IMG_3374.JPG";
+import IMG_3375 from "./assets/IMG_3375.JPG";
+import IMG_3377 from "./assets/IMG_3377.JPG";
+import IMG_3381 from "./assets/IMG_3381.JPG";
+import IMG_3385 from "./assets/IMG_3385.JPG";
+import IMG_3387 from "./assets/IMG_3387.JPG";
+import IMG_3388 from "./assets/IMG_3388.JPG";
+import IMG_3389 from "./assets/IMG_3389.JPG";
+import IMG_3390 from "./assets/IMG_3390.JPG";
+import IMG_3392 from "./assets/IMG_3392.JPG";
+
 const IMAGES = [
-  "/images/IMG_3394.JPG",
-  "/images/IMG_0412.jpg",
-  "/images/IMG_2981.HEIC",
-  "/images/IMG_3008.PNG",
-  "/images/IMG_3368.JPG",
-  "/images/IMG_3372.JPG",
-  "/images/IMG_3373.JPG",
-  "/images/IMG_3374.JPG",
-  "/images/IMG_3375.JPG",
-  "/images/IMG_3377.JPG",
-  "/images/IMG_3381.JPG",
-  "/images/IMG_3385.JPG",
-  "/images/IMG_3387.JPG",
-  "/images/IMG_3388.JPG",
-  "/images/IMG_3389.JPG",
-  "/images/IMG_3390.JPG",
-  "/images/IMG_3392.JPG",
-  // "/images/coverflow--Out-Of-the-Dark-Cover-550x550.jpg",
-  // "/images/coverflow--2020-06-07-Marley-Carroll-Single-Blue-550x550.jpg",
-  // "/images/coverflow--Deep-Dive-EP-Cover-550x550.jpg",
-  // "/images/coverflow--Out-Of-the-Dark-Cover-550x550.jpg",
-  // "/images/coverflow--2020-06-07-Marley-Carroll-Single-Blue-550x550.jpg",
-  // "/images/coverflow--Deep-Dive-EP-Cover-550x550.jpg",
-  // "/images/coverflow--Out-Of-the-Dark-Cover-550x550.jpg",
-  // "/images/coverflow--2020-06-07-Marley-Carroll-Single-Blue-550x550.jpg",
-  // "/images/coverflow--Deep-Dive-EP-Cover-550x550.jpg",
+  IMG_3394,
+  IMG_0412,
+  IMG_3008,
+  IMG_3368,
+  IMG_3372,
+  IMG_3373,
+  IMG_3374,
+  IMG_3375,
+  IMG_3377,
+  IMG_3381,
+  IMG_3385,
+  IMG_3387,
+  IMG_3388,
+  IMG_3389,
+  IMG_3390,
+  IMG_3392,
 ] as const;
 
 export default function CoverFlow() {
