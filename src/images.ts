a1831f044f7f0ff634769f6add301e8074865824
Siyaman <siyaman.sadagopan@bytedance.com>
2025-12-04 18:12:48 -0800

    Move images to public folder

--- src/images.ts
+++ src/images.ts
@@ -1,26 +1,14 @@
-import Photos_Of_Weddings_7 from "./assets/Photos-Of-Weddings-7.jpg.webp";
-import Photos_Of_Weddings_17 from "./assets/Photos-Of-Weddings-17.jpg.webp";
-import Photos_Of_Weddings_21 from "./assets/Photos-Of-Weddings-21.jpg.webp";
-import Photos_Of_Weddings_27 from "./assets/Photos-Of-Weddings-27.jpg.webp";
-import Photos_Of_Weddings_33 from "./assets/Photos-Of-Weddings-33.jpg.webp";
-import Photos_Of_Weddings_37 from "./assets/Photos-Of-Weddings-37.jpg.webp";
-import Photos_Of_Weddings_46 from "./assets/Photos-Of-Weddings-46.jpg.webp";
-import Photos_Of_Weddings_102 from "./assets/Photos-Of-Weddings-102.jpg.webp";
-import Photos_Of_Weddings_109 from "./assets/Photos-Of-Weddings-109.jpg.webp";
-import Photos_Of_Weddings_118 from "./assets/Photos-Of-Weddings-118.jpg.webp";
-import Photos_Of_Weddings_137 from "./assets/Photos-Of-Weddings-137.jpg.webp";
-
 const IMAGES = [
-  Photos_Of_Weddings_7,
-  Photos_Of_Weddings_17,
-  Photos_Of_Weddings_21,
-  Photos_Of_Weddings_27,
-  Photos_Of_Weddings_33,
-  Photos_Of_Weddings_37,
-  Photos_Of_Weddings_46,
-  Photos_Of_Weddings_102,
-  Photos_Of_Weddings_109,
-  Photos_Of_Weddings_118,
-  Photos_Of_Weddings_137,
+  "./assets/Photos-Of-Weddings-7.jpg.webp",
+  "./assets/Photos-Of-Weddings-17.jpg.webp",
+  "./assets/Photos-Of-Weddings-21.jpg.webp",
+  "./assets/Photos-Of-Weddings-27.jpg.webp",
+  "./assets/Photos-Of-Weddings-33.jpg.webp",
+  "./assets/Photos-Of-Weddings-37.jpg.webp",
+  "./assets/Photos-Of-Weddings-46.jpg.webp",
+  "./assets/Photos-Of-Weddings-102.jpg.webp",
+  "./assets/Photos-Of-Weddings-109.jpg.webp",
+  "./assets/Photos-Of-Weddings-118.jpg.webp",
+  "./assets/Photos-Of-Weddings-137.jpg.webp",
 ] as const;
 export default IMAGES;
