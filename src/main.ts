dc60dd3106908ce06052bedb3630eefea5ad4fc3
Siyaman <siyaman.sadagopan@bytedance.com>
2025-12-10 17:51:18 -0800

    Assert that root element always exists in HTML
    Otherwise crash immediately

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createElement, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import CoverFlow from "./CoverFlow.tsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   createElement(StrictMode, null, createElement(CoverFlow)),
 );
